16c6154fb9734e7adaeb11b1e078a29afc1185d2
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-02-17 22:04:24 +0100

    load-from-url

---
+++ README.md
@@ -0,0 +1,14 @@
+# The [Parsonizer](https://janke-learning.github.io/parsonizer/)
+
+Parsons problems are designed to help you study snippets of code without getting bogged down in errors and syntax.  Simply paste in the code you want to study, and the parsonizer will shuffle the lines.  Your task is to place them in order.  But it's not always as easy as it sounds!  Beware of indenting (code style) and extra lines.
+
+When you find a particularly interesting example of JavaScript syntax, design pattern, solution strategy, copy paste it into [this web page](https://janke-learning.github.io/parsonizer/) to study it in depth as a parsons problem.
+
+Parsons problems are a great way to practice syntax, logical strategies, and programming design patterns.
+
+> Powered by [js-parsons](https://github.com/js-parsons/js-parsons)
+
+___
+___
+### <a href="http://janke-learning.org" target="_blank"><img src="https://user-images.githubusercontent.com/18554853/50098409-22575780-021c-11e9-99e1-962787adaded.png" width="40" height="40"></img> Janke Learning</a>
+
---
+++ load-from-url.js
@@ -0,0 +1,45 @@
+const loadFromURL = () => {
+  const url = new URL(window.location.href);
+  const code = url.searchParams.get("code");
+  const snippet = url.searchParams.get("snippet");
+  console.log(url)
+
+  let parsonsCode;
+  if (code !== null) {
+    // console.log(1)
+    const decoded = decodeURIComponent(code);
+    parsonsCode = decoded.replace(/\%28/g, '(').replace(/\%29/g, ')');
+  } else if (snippet !== null) {
+    // console.log(2)
+    const decoded = decodeURIComponent(snippet);
+    parsonsCode = decoded.replace(/\%28/g, '(').replace(/\%29/g, ')');
+  } else {
+    // console.log(4)
+    parsonsCode = 'const x = "y";';
+  };
+
+  function displayErrors(fb) {
+    if (fb.errors.length > 0) {
+      alert(fb.errors[0]);
+    }
+  }
+  const parsonsInstance = new ParsonsWidget({
+    'sortableId': 'sortable',
+    'trashId': 'sortableTrash',
+    // 'max_wrong_lines': 1, // ?
+    'feedback_cb': displayErrors,
+    'first_error_only': false,
+  });
+
+  parsonsInstance.init(parsonsCode);
+
+  parsonsInstance.shuffleLines();
+  $("#newInstanceLink").click(function (event) {
+    event.preventDefault();
+    parsonsInstance.shuffleLines();
+  });
+  $("#feedbackLink").click(function (event) {
+    event.preventDefault();
+    parsonsInstance.getFeedback();
+  });
+}
